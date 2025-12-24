d0c7c1065a6c4b0fe4a31f0c693fade245899046
MishSoft <michaelmakaveli123@gmail.com>
2025-12-24 14:56:07 +0400

    Finished Project

--- src/app/components/Hero/HeroBtns.tsx
+++ src/app/components/Hero/HeroBtns.tsx
@@ -2,19 +2,27 @@ import React from 'react'
 import { ArrowDownToLine, ChevronDown } from "lucide-react"
 import Link from 'next/link'
 
-
 export default function HeroBtns() {
   return (
     <div className='flex flex-col md:flex-row items-center gap-5 my-10'>
-      <Link href='#work' className='px-5 md:px-10 md:py-4 text-white hover:bg-blue-700 duration-200 cursor-pointer py-2 flex items-center gap-2 bg-blue-500 rounded-xl'>
+      {/* ღილაკი: ნავიგაცია სამუშაოებზე */}
+      <Link
+        href='#work'
+        className='px-5 md:px-10 md:py-4 text-white hover:bg-blue-700 duration-200 cursor-pointer py-2 flex items-center gap-2 bg-blue-500 rounded-xl'
+      >
         View Work
         <ChevronDown size={20} />
       </Link>
 
-      <button className='px-5 md:px-10 md:py-4 text-white flex items-center gap-2 py-2 bg-gray-700 hover:bg-gray-800 duration-200 cursor-pointer border border-gray-500 rounded-xl'>
+      {/* ღილაკი: CV-ს ჩამოტვირთვა */}
+      <a
+        href="/Mishiko-Aspanidze-resume-2025.pdf" // ფაილის სახელი public ფოლდერიდან
+        download="Mishiko_Aspanidze_CV.pdf" // სახელი, რომლითაც ჩამოიტვირთება
+        className='px-5 md:px-10 md:py-4 text-white flex items-center gap-2 py-2 bg-gray-700 hover:bg-gray-800 duration-200 cursor-pointer border border-gray-500 rounded-xl'
+      >
         <ArrowDownToLine size={20} />
         Resume
-      </button>
+      </a>
     </div>
   )
 }
